bfaf292171a88ce028dc96d64fd32cd670829c48
atharvapatil123 <atharva7275@gmail.com>
2023-02-16 17:07:34 +0530

    Editing SC

--- contracts/SmartEstate.sol
+++ contracts/SmartEstate.sol
@@ -37,7 +37,7 @@ contract SmartEstate {
         uint256 timestamp;
         uint256 plotId;
         int8 state; // 0:PENDING / 1:VALIDATED / 2:REJECTED
-        bool transacType; // 0: sell 1: buy
+        int8 transacType; // 0: sell 1: buy
     }
 
     // struct Pending {
@@ -71,12 +71,32 @@ contract SmartEstate {
 
     mapping(address => uint256) userAddressToIdMapping;
 
+    function checkAvailableStocksForSeller(uint256 stockId, uint256 sellable)
+        public
+        view
+        returns (bool)
+    {
+        if (stocks[stockId].sellable + sellable <= stocks[stockId].quantity)
+            return true;
+        else return false;
+    }
+
+    function checkAvailableStocksForBuyer(uint256 stockId, uint256 buyable)
+        public
+        view
+        returns (bool)
+    {
+        if (stocks[stockId].sellable - buyable >= 0) return true;
+        else return false;
+    }
+
     // mapping(uint256 => uint256)
 
-    function compareStrings(
-        string memory _string1,
-        string memory _string2
-    ) public pure returns (bool) {
+    function compareStrings(string memory _string1, string memory _string2)
+        public
+        pure
+        returns (bool)
+    {
         return (keccak256(abi.encodePacked((_string1))) ==
             keccak256(abi.encodePacked((_string2))));
     }
@@ -168,7 +188,9 @@ contract SmartEstate {
             quantity: plots[plotCount].totalQuantity,
             sellable: plots[plotCount].availableStocks
         });
+        // 1- 100, 80
 
+        stockCount += 1;
         plotCount += 1;
 
         plotRequests[plotId] = plotRequests[plotRequestCount - 1];
@@ -178,10 +200,152 @@ contract SmartEstate {
     }
 
     function updateSellableStocks(uint256 stockId, uint256 sellable) public {
-        require(
-            stocks[stockId].sellable + sellable <= stocks[stockId].quantity,
-            "Sellable quantity is more than the total stocks that you have!"
-        );
         stocks[stockId].sellable += sellable;
+    }
+
+    function updateBuyableStocks(uint256 stockId, uint256 buyable) public {
+        stocks[stockId].sellable -= buyable;
+        stocks[stockId].quantity -= buyable;
+        // 1 100 80-30=50
+    }
+
+    function createTransaction(
+        uint256 source,
+        uint256 target,
+        uint256 quantity,
+        uint256 price,
+        uint256 timestamp,
+        uint256 plotId,
+        // int8 state,
+        int8 transacType
+    ) public returns (uint256) {
+        transactions[transactionCount] = Transaction({
+            source: source,
+            target: target,
+            quantity: quantity,
+            price: price,
+            timestamp: timestamp,
+            plotId: plotId,
+            state: 0,
+            transacType: transacType
+        });
+        // validateTransaction(transactionCount);
+
+        transactionCount += 1;
+        return transactionCount - 1;
+    }
+
+    function validateTransaction(uint256 transacId) public {
+        if (transactions[transacId].state == 0)
+            transactions[transacId].state = 1;
+    }
+
+    function rejectTransaction(uint256 transacId) public {
+        if (transactions[transacId].state == 0)
+            transactions[transacId].state = 2;
+    }
+
+    // REQUEST TO BUY
+
+    function buyStocks(
+        uint256 stockId,
+        address target,
+        uint256 quantityToBuy,
+        uint256 price,
+        uint256 plotId,
+        uint256 sellQuantity
+    ) public {
+        uint256 transacId = createTransaction(
+            userAddressToIdMapping[msg.sender],
+            userAddressToIdMapping[target],
+            quantityToBuy,
+            price,
+            block.timestamp,
+            plotId,
+            1
+        );
+        if (!checkAvailableStocksForBuyer(stockId, quantityToBuy))
+            rejectTransaction(transacId);
+        else {
+            uint256 i = 0;
+            for (i = 0; i < stockCount; i++) {
+                if (stocks[i].userId == userAddressToIdMapping[msg.sender]) {
+                    if (stocks[i].quantity + quantityToBuy > sellQuantity) {
+                        stocks[i].quantity += quantityToBuy;
+                        stocks[i].sellable += quantityToBuy;
+                        updateBuyableStocks(stockId, quantityToBuy);
+                        validateTransaction(transacId);
+                    } else {
+                        rejectTransaction(transacId);
+                    }
+                }
+            }
+            if (i == stockCount) {
+                if (quantityToBuy > sellQuantity) {
+                    updateBuyableStocks(stockId, quantityToBuy);
+                    stocks[stockCount] = Stocks({
+                        userId: userAddressToIdMapping[msg.sender],
+                        plotId: plotId,
+                        quantity: quantityToBuy,
+                        sellable: sellQuantity
+                    });
+                    stockCount += 1;
+                    validateTransaction(transacId);
+                } else {
+                    rejectTransaction(transacId);
+                }
+            }
+        }
+    }
+
+    function sellStocks(
+        uint256 stockId,
+        address target,
+        uint256 quantityToSell,
+        uint256 price,
+        uint256 plotId,
+        uint256 sellQuantity
+    ) public {
+        uint256 transacId = createTransaction(
+            userAddressToIdMapping[msg.sender],
+            userAddressToIdMapping[target],
+            quantityToSell,
+            price,
+            block.timestamp,
+            plotId,
+            0
+        );
+        if (!checkAvailableStocksForBuyer(stockId, quantityToSell))
+            rejectTransaction(transacId);
+        else {
+            uint256 i = 0;
+            for (i = 0; i < stockCount; i++) {
+                if (stocks[i].userId == userAddressToIdMapping[msg.sender]) {
+                    if (stocks[i].quantity + quantityToBuy > sellQuantity) {
+                        stocks[i].quantity += quantityToBuy;
+                        stocks[i].sellable += quantityToBuy;
+                        updateBuyableStocks(stockId, quantityToBuy);
+                        validateTransaction(transacId);
+                    } else {
+                        rejectTransaction(transacId);
+                    }
+                }
+            }
+            if (i == stockCount) {
+                if (quantityToBuy > sellQuantity) {
+                    updateBuyableStocks(stockId, quantityToBuy);
+                    stocks[stockCount] = Stocks({
+                        userId: userAddressToIdMapping[msg.sender],
+                        plotId: plotId,
+                        quantity: quantityToBuy,
+                        sellable: sellQuantity
+                    });
+                    stockCount += 1;
+                    validateTransaction(transacId);
+                } else {
+                    rejectTransaction(transacId);
+                }
+            }
+        }
     }
 }
