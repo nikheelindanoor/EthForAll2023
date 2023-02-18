dad8ce5c2c50610c6aa2e7af0721ed634255ba96
atharvapatil123 <atharva7275@gmail.com>
2023-02-19 01:05:25 +0530

    Plots page done

--- src/pages/Plots/Plots.jsx
+++ src/pages/Plots/Plots.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import styles from "./Plots.module.css";
 import { useNavigate } from "react-router-dom";
 import { useCallback, useState, useEffect, useRef } from "react";
+import Modal from "react-modal";
+// import CloseIcon from "@mui/icons-material/Close";
+import MoonLoader from "react-spinners/MoonLoader";
 
 const Plots = () => {
   const navigate = useNavigate();
@@ -34,9 +37,156 @@ const Plots = () => {
     },
   ]);
 
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [stockId, setStockId] = useState();
+  const [address, setAddress] = useState("");
+  const [buyQuantity, setBuyQuantity] = useState();
+  const [buyPrice, setBuyPrice] = useState();
+  const [plotId, setPlotId] = useState();
+  const [sellQuantity, setSellQuantity] = useState();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [modalIsOpen2, setModalIsOpen2] = useState(false);
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+  function closeModal2() {
+    setModalIsOpen2(false);
+  }
+
+  const openModal = async (e) => {
+    e.preventDefault();
+    setModalIsOpen(true);
+  };
+  const openModal2 = async (e) => {
+    e.preventDefault();
+    setModalIsOpen2(true);
+  };
+
   return (
     <>
       <div className={styles.detailsBox}>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          contentLabel="Buy stocks"
+          style={{
+            content: {
+              top: "50%",
+              left: "50%",
+              right: "auto",
+              bottom: "auto",
+              marginRight: "-50%",
+              transform: "translate(-50%, -50%)",
+            },
+          }}
+        >
+          <div className={styles.modalContainer}>
+            <button className={styles.closeButton} onClick={closeModal}>
+              {/* <CloseIcon /> */}
+            </button>
+            <h2
+              className={styles.heading}
+              style={{
+                width: "100%",
+                textAlign: "center",
+              }}
+            >
+              Details
+            </h2>
+
+            <form className={styles.formBox}>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="number"
+                  placeholder="Stock ID"
+                  onChange={(e) => setStockId(e.target.value)}
+                  value={stockId}
+                />
+              </div>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="text"
+                  placeholder="Creator's address"
+                  onChange={(e) => setAddress(e.target.value)}
+                  value={address}
+                />
+              </div>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="number"
+                  placeholder="Quantity to buy"
+                  onChange={(e) => setBuyQuantity(e.target.value)}
+                  value={buyQuantity}
+                />
+              </div>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="number"
+                  placeholder="Price"
+                  onChange={(e) => setBuyPrice(e.target.value)}
+                  value={buyPrice}
+                />
+              </div>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="number"
+                  placeholder="Plot ID"
+                  onChange={(e) => setPlotId(e.target.value)}
+                  value={plotId}
+                />
+              </div>
+              <div className={styles.inputGroup}>
+                <input
+                  className={`${styles.input}`}
+                  style={{
+                    resize: "none",
+                  }}
+                  type="number"
+                  placeholder="Sell Quantity"
+                  onChange={(e) => setSellQuantity(e.target.value)}
+                  value={sellQuantity}
+                />
+              </div>
+
+              <button
+                className={`${styles.modalIssueBtn}`}
+                // onClick={handleSubmit}
+              >
+                {isLoading ? (
+                  <MoonLoader
+                    className={styles.loader}
+                    color="white"
+                    size={20}
+                  />
+                ) : (
+                  "Buy"
+                )}
+              </button>
+            </form>
+          </div>
+        </Modal>
         <span className={styles.heading}>All Plots</span>
         <div className={styles.eventPageBody}>
           {/* <div className={styles.searchEventsContainer}>
@@ -163,7 +313,7 @@ const Plots = () => {
                       <div>
                         <button
                           onClick={(e) => {
-                            // openModal(e);
+                            openModal(e);
                             // set_ReqId(request.reqId);
                           }}
                           className={styles.issueBtn}
